83c0cc397985d3059b1cea0c06efd9a84a055e5d
cdangeti <charandangeti7@gmail.com>
2024-10-09 20:33:55 -0400

    Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header className="navbar">
             <h1 className="logo">Launchpad sites</h1>
+        <img src="https://mail.google.com/mail/u/0/?ui=2&ik=14d50f149c&attid=0.1&permmsgid=msg-f:1812481502816407734&th=19273a52e95690b6&view=fimg&disp=thd&attbid=ANGjdJ_wtX0WG9kyMz6gG2XBj1o7owxQjEaFa5NbomB281ylEyq3txUBM9r3SwSb_ZcmzyMjESKHxTtCt-eKrZq6ZTr0yy8OsRuejk5Zr1Mgh4PN90bPjY7QXpJmVSU&ats=2524608000000&sz=w1920-h877">
             <nav>
                 <ul className="nav-links">
                     <li><Link to="/">Home</Link></li>
