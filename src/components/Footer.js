8c730e915eeb830c8167e8b469e2d737d677f1f7
cdangeti <charandangeti7@gmail.com>
2024-10-09 20:48:25 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p class="footer">&copy; 2024 Company Launchpad. All rights reserved.</p>
+            <p classNAME="footer">&copy; 2024 Company Launchpad. All rights reserved.</p>
         </footer>
     );
 };
