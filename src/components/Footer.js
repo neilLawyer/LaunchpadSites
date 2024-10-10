1d240becbbc918ff89c74cf39d7631ae949e2050
cdangeti <charandangeti7@gmail.com>
2024-10-10 17:53:44 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './Footer.css'; // Importing a CSS file for styles
+
+const Footer = () => {
+    return (
+<footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '20px' }}>
+    <p>&copy; 2024 Company Launchpad. All rights reserved.</p>
+</footer>
+    );
+};
+
+
+export default Footer;
 
